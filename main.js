5a3c91d00b0a873e745846775b0a668d96e5d1e5
Antoine Dricot <antdricot@gmail.com>
2022-08-17 20:55:52 +0200

    add style.css and main.js

---
+++ main.js
@@ -0,0 +1,27 @@
+// Death
+// MIT License - Copyright 2022 Antoine Dricot
+// A dumb and charming game.
+
+// TODO
+// 1) ?
+function main() {
+    // -- Canvas
+    let mainCanvas = document.createElement("canvas");
+    // let actualHeight = window.innerHeight;
+    mainCanvas.height = 100;
+    mainCanvas.width = 100;
+    document.body.appendChild(mainCanvas);
+
+    // // -- Control
+    // document.addEventListener("keydown", keyDownHandler, false);
+    // document.addEventListener("keyup", keyUpHandler, false);
+    // document.addEventListener("touchstart", touchDownHandler, false);
+    // document.addEventListener("touchend", touchUpDownHandler, false);
+
+    // Run
+    // let mainGame = new Game(mainCanvas);
+    // mainGame.initialize();
+    // mainGame.run();
+}
+
+main();
